a0d6a1cfc5644a6620963cc6236e43b6a9d0ff28
waqar1104 <waqar.slamslogic@gmail.com>
2019-08-02 22:28:41 +0500

    at 10:28

--- src/test/java/getRequest/GetData.java
+++ src/test/java/getRequest/GetData.java
@@ -1,7 +1,9 @@
 package getRequest;
 
 import static io.restassured.RestAssured.*;
+import static io.restassured.path.json.JsonPath.from;
 import io.restassured.response.Response;
+import java.util.List;
 import org.apache.http.HttpStatus;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,22 +14,38 @@ import org.testng.annotations.Test;
  */
 public class GetData {
 
+    String token = "00D4B0000000iK2!AQ4AQJL5AlALALX72TFov9M5ii63UnyZk_PeL5Jaf.tOVk.ZIsUgGxRm7cgm2Qdn8QTwl3W4f58gD9lnE.Iwn2CkRTHtf4cS";
+
+    String instanceUrl = "https://cs20.salesforce.com";
+
     @Test
-    public void testResponsecode() {
-        Response resp = get("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+    public void testShipToAddr() {
+        Response resp = given().header("access_token", token).header("instance_url", instanceUrl).
+                when().
+                contentType("application/json").
+                get("https://api.rootscan.app/v1/mobile/c1/sobjects/rstk__sohdr__c/action/new/rstk__solines__r/rstk__soline_oshiptoaddr__c");
+
         int code = resp.getStatusCode();
-        System.out.println("Status code is " + code);
+        String responseBody = resp.asString();
+        System.out.println("======= \n Status code is " + code);
+        System.out.println("======= \n Response body is " + responseBody);
+        System.out.println("\n=======");
 
         Assert.assertEquals(code, HttpStatus.SC_OK);
     }
 
     @Test
-    public void testResponsecode1() {
-        Response resp = get("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+    public void testSohdrContact() {
+        Response resp = given().header("access_token", token).header("instance_url", instanceUrl).
+                when().
+                contentType("application/json").
+                get("https://api.rootscan.app/v1/mobile/c1/sobjects/rstk__sohdr__c/action/new/rstk__sohdr_contact__c");
         int code = resp.getStatusCode();
-        System.out.println("Status code is " + code);
+        String responseBody = resp.asString();
+        System.out.println("======= \n Status code is " + code);
+        System.out.println("======= \n Response body is " + responseBody);
+        System.out.println("\n=======");
 
-        Assert.assertEquals(code, HttpStatus.SC_FORBIDDEN);
+        Assert.assertEquals(resp.jsonPath().getString("status"), HttpStatus.SC_OK);
     }
-
 }
